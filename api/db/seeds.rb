1362f59e10adbac2f6459e3218b265d1090bc594
waterlinux <water@gmail.com>
2024-10-07 15:53:50 +0200

    final commit

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -8,3 +8,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Clear existing data
+Category.destroy_all
+Video.destroy_all
+
+# Create some categories
+categories = Category.create!([
+  { name: 'Music' },
+  { name: 'Education' },
+  { name: 'Entertainment' },
+  { name: 'Technology' }
+])
+
+# Create some videos linked to the categories
+Video.create!([
+  { title: 'How to Play Guitar', categories_id: categories[0].id },
+  { title: 'Ruby on Rails Tutorial', categories_id: categories[1].id },
+  { title: 'Comedy Sketch', categories_id: categories[2].id },
+  { title: 'The Future of AI', categories_id: categories[3].id }
+])
+
+puts "Seeding completed successfully!"
